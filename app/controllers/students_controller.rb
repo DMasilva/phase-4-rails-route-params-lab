a3ea516dc3ebe4a04a63feea598d5159b38fbba0
David <david.owuor@student.moringaschool.com>
2023-03-13 23:22:08 +0300

    complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,7 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
-
+  def show
+    render json: Student.all.find_by(id: params[:id])
+  end
 end
